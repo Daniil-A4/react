20f2bf58e0587444366c779b4ce7452ed04e3748
Daniil <danil.beshlebnov@gmail.com>
2023-10-23 15:36:35 +0300

    clear code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
-import React, { useState } from 'react';
+// import React from 'react';
 import './app.css';
-import { PostItem } from './components/PostItem/PostItem';
 
 function App() {
-  const [posts] = useState(
-    [
-      {
-        id: 1,
-        title: "post's title 1",
-        descr: 'description',
-      },
-      {
-        id: 2,
-        title: "post's title 2",
-        descr: 'description',
-      },
-      {
-        id: 3,
-        title: "post's title 3",
-        descr: 'description',
-      },
-    ]
-  );
-
-  return (
-    <div className='container'>
-      <ul className='posts'>
-        {posts.map((post) => (
-          <PostItem post={post} key={post.id} />
-        ))}
-      </ul>
-    </div>
-  );
 }
 
 export default App;
--- src/components/PostItem/PostItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const PostItem = (post) => {
-  <li key={post.id} className='postItem'>
-    <h2 className='postItem__title'>{post.title}</h2>
-    <p className='postItem__descr'>{post.descr}</p>
-    <button className='postItem__btn'>Delete</button>
-  </li>
-}
